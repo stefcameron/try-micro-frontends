3411b74f55d37f5d7ed696036b976dec7ad854f3
Stefan Cameron <stefan@stefcameron.com>
2024-04-22 17:53:30 -0500

    Add WP dev server proxy routes to SWAPI

--- apps/app1/webpack.config.mjs
+++ apps/app1/webpack.config.mjs
@@ -9,4 +9,21 @@
 
 import { mkConfig } from '../../tools/build/wpBuilds.mjs';
 
-export default mkConfig();
+const config = mkConfig();
+
+config.devServer.proxy = [
+  {
+    context: [
+      '/api/films',
+      '/api/people',
+      '/api/species',
+      '/api/planets',
+      '/api/starships',
+      '/api/vehicles',
+    ],
+    target: 'https://swapi.dev',
+    secure: false,
+  },
+];
+
+export default config;
